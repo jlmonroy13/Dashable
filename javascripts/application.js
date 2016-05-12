da2afa10bb563fc221be1ee5611a1181de0892ad
jlmonroy13 <jlmonroy13@gmail.com>
2016-05-12 18:04:28 -0500

    markup checkin responsive ok

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,3 +1,36 @@
 $(document).ready(function(){
-  $(".owl-carousel").owlCarousel();
+  var $input      =     $('.input');
+
+  $input.blur(function() {
+    if( $(this).val() !== '') {
+      $(this).next('.input__label').addClass('active-input-js');
+    } else {
+      $(this).next('.input__label').removeClass('active-input-js');
+    }
+  });   
+  $(".owl-carousel").owlCarousel({
+    loop: false,
+    margin: 5,
+    startPosition: 6,
+    responsive:{
+     0:{
+       items:6,
+       nav:true,
+       dots: false
+     },
+     480:{
+       items:6,
+       dots: false,
+     },
+     768:{
+       items:6,
+       nav:false,
+       dots: false,
+     },
+     1024:{
+       items:6,
+       nav:true,
+     },
+   }
+ });
 }); 
